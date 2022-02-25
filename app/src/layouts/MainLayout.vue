e72296f002926ea309f1ad56382fe86179b1c30e
Luiz André Tavares <luizandre.tavares@outlook.com>
2022-02-25 14:30:04 -0300

    escopo

--- app/src/layouts/MainLayout.vue
+++ app/src/layouts/MainLayout.vue
@@ -1,43 +1,16 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+      <q-toolbar class="q-ma-md">
+        <q-toolbar-title> PORTAL Extensão UFRJ </q-toolbar-title>
+        <q-search v-model="search"></q-search>
+        <q-btn flat label="Destaques"> </q-btn>
+        <q-btn flat label="Vagas"> </q-btn>
+        <q-btn flat label="Lives"> </q-btn>
+        <q-btn flat label="Informações"> </q-btn>
+        <q-btn flat label="Notícias"> </q-btn>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -46,72 +19,15 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
+  name: "MainLayout",
+  data: function () {
+    return { search: "" };
   },
+  components: {},
 
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+  setup() {},
+});
 </script>
--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -1,17 +1,215 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div>
+    <q-page class="hero">
+      <q-carousel
+        v-model="slide"
+        transition-prev="jump-right"
+        transition-next="jump-left"
+        swipeable
+        animated
+        control-color="white"
+        prev-icon="arrow_left"
+        next-icon="arrow_right"
+        navigation-icon="radio_button_unchecked"
+        navigation
+        padding
+        arrows
+        class="bg-purple shadow-1"
+      >
+        <q-carousel-slide name="style" class="row no-wrap">
+          <div class="col-7 column justify-center">
+            <p class="col-3 text-h1 text-white">Última Newsletter</p>
+            <p class="col-2 text-h4 text-white q-pa-md">
+              Acesse nossa newsletter e fique por dentro de tudo que aconteceu
+              no mês
+            </p>
+            <q-btn
+              flat
+              class="col-1"
+              label="Inscreva-se Aqui"
+              color="green"
+            ></q-btn>
+          </div>
+          <q-img
+            id="comunica-img"
+            class="col-5 rounded-borders"
+            src="images/comunica-01-2022.png"
+            fit="fill"
+          >
+          </q-img>
+        </q-carousel-slide>
+        <q-carousel-slide name="tv" class="column no-wrap flex-center">
+          <q-icon name="live_tv" />
+          <div class="q-mt-md text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+    </q-page>
+
+    <q-page class="column q-pa-xl">
+      <span class="col-1 text-h2">Ações em Destaque</span>
+      <div class="col row q-pa-xl items-center">
+        <div class="col" v-for="acao in acoes" :key="acao">
+          <div>
+            <q-tooltip class="text-h4 q-pa-xl q-ma-xl">
+              Cursos para complementar sua formação acadêmica e cidadã O Curso
+              de Extensão é responsável por garantir uma formação livre em um
+              determinado tema e é oferecido tanto para o público externo,
+              quanto para a universidade. Um Curso pode ser presencial ou à
+              distância, teórico ou prático e ter a duração de semanas ou meses,
+              a depender de cada coordenação.
+            </q-tooltip>
+            <q-chip
+              square
+              :color="acao.cor"
+              text-color="white"
+              icon="alarm"
+              :label="acao.modalidade"
+            />
+          </div>
+          <q-card flat class="my-card" :style="'border-color: ' + acao.cor">
+            <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+            <q-card-section>
+              <div class="text-h6">{{ acao.titulo }}</div>
+              <div class="text-subtitle2">{{ acao.coordenador }}</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              {{ acao.descricao }}
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+      <q-btn class="col-1" label="Veja todas as ações"></q-btn>
+    </q-page>
+
+    <q-page class="column bg-yellow q-pa-xl">
+      <span class="col-1 text-h2">Vagas em Destaque</span>
+      <div class="col row q-pa-xl items-center">
+        <div class="col" v-for="acao in acoes" :key="acao">
+          <q-card
+            flat
+            vertical
+            class="my-card"
+            :style="'border-color: ' + acao.cor"
+          >
+            <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+            <q-card-section>
+              <div class="text-h6">{{ acao.titulo }}</div>
+              <div class="text-subtitle2">{{ acao.coordenador }}</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              {{ acao.descricao }}
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+      <q-btn class="col-1" label="Veja todas as vagas"></q-btn>
+    </q-page>
+
+    <q-page class="column bg-green q-pa-xl">
+      <span class="col-1 text-h2">Lives em Destaque</span>
+      <q-tabs class="col-1">
+        <q-tab>Ao vivo</q-tab>
+        <q-tab>Próximas</q-tab>
+        <q-tab>Passadas</q-tab>
+      </q-tabs>
+      <div class="col row q-pa-xl items-center">
+        <div class="col" v-for="acao in acoes" :key="acao">
+          <q-card
+            flat
+            vertical
+            class="my-card"
+            :style="'border-color: ' + acao.cor"
+          >
+            <iframe
+              width="400"
+              height="315"
+              src="https://www.youtube.com/embed/Xz3bMCj6BFI"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+
+            <q-card-section>
+              <div class="text-h6">{{ acao.titulo }}</div>
+              <div class="text-subtitle2">{{ acao.coordenador }}</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              {{ acao.descricao }}
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+      <q-btn class="col-1" label="Veja todas as Lives"></q-btn>
+    </q-page>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  name: "PageIndex",
+  data: function () {
+    return {
+      slide: "style",
+      acoes: [
+        {
+          titulo: "América Latina Póscolonial: estudos de caso.",
+          coordenador: "CARLOS EDUARDO REBELLO DE MENDONCA",
+          modalidade: "Curso",
+          cor: "red",
+          descricao:
+            "Trata-se de curso onde a história política, social e econômica recente da América Latina será discutida a partir dos enfoques trazidos pela perspectiva dos estudos pós-coloniais e subalternistas, através da leitura de alguns clássicos e da literatura recente sobre alguns casos nacionais mais relevantes",
+        },
+        {
+          titulo: "América Latina Póscolonial: estudos de caso.",
+          coordenador: "CARLOS EDUARDO REBELLO DE MENDONCA",
+          modalidade: "Evento",
+          cor: "green",
+
+          descricao:
+            "Trata-se de curso onde a história política, social e econômica recente da América Latina será discutida a partir dos enfoques trazidos pela perspectiva dos estudos pós-coloniais e subalternistas, através da leitura de alguns clássicos e da literatura recente sobre alguns casos nacionais mais relevantes",
+        },
+        {
+          titulo: "América Latina Póscolonial: estudos de caso.",
+          coordenador: "CARLOS EDUARDO REBELLO DE MENDONCA",
+          modalidade: "Projeto",
+          cor: "blue",
+
+          descricao:
+            "Trata-se de curso onde a história política, social e econômica recente da América Latina será discutida a partir dos enfoques trazidos pela perspectiva dos estudos pós-coloniais e subalternistas, através da leitura de alguns clássicos e da literatura recente sobre alguns casos nacionais mais relevantes",
+        },
+      ],
+    };
+  },
+});
 </script>
+
+<style scoped>
+.hero {
+  background-size: 100% 100%;
+}
+
+.q-carousel {
+  min-height: inherit;
+  min-width: inherit;
+}
+
+.rounded-borders {
+  border-radius: 5%;
+}
+
+.my-card {
+  width: 100%;
+  max-width: 400px;
+  border-style: outset;
+}
+</style>
