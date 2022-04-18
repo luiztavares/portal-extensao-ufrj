d0320a1a6f19860819ca1629cb3bcf8647bb498d
Luiz André Tavares <luizandre.tavares@outlook.com>
2022-04-18 09:26:05 -0300

    nova home

--- app/src/components/HomeCard.vue
+++ app/src/components/HomeCard.vue
@@ -1,19 +1,15 @@
 <template>
-  <q-card  class="my-card rounded-borders">
+  <q-card  class="mycard">
     <router-link v-ripple class="cursor-pointer relative-position" :to="'/'+ acao.attributes.modalidade+'/' + acao.id ">
-    <q-img class='rounded-borders' v-if='vaga == false'  :src="meta[acao.attributes.modalidade].imagem+(index%3+1)">
-      <p class="q-ml-xl q-pl-sm q-mt-md q-pt-md q-pr-sm text-left text-h6" :style='"border-left: 3px solid "+ meta[acao.attributes.modalidade].corTitulo +";" + "color: "+ meta[acao.attributes.modalidade].corTitulo'>
-        {{ dropTitle(acao.attributes.titulo) }}
-      </p>
-      <p class=" q-ml-xl q-pl-sm text-left text-h7" :style='"color: "+ meta[acao.attributes.modalidade].cor'>
-        {{ acao.attributes.coordenador_da_acao_de_extensao }}
-      </p>
-
-      <p class="q-ml-xl q-mt-xl q-pt-xl q-pl-sm  text-left text-h7" :style='"color: "+ meta[acao.attributes.modalidade].cor'>
-        Endereço: {{ acao.attributes.endereco }}
-      </p>
+    <q-img class='' v-if='vaga == false'  :src="acao.attributes.imagem">
     </q-img>
-        <q-img class='rounded-borders' v-if='vaga == true'  :src="'images/vagas'+(index%3+1)">
+      <q-card-section >
+        <div class="text-h6 text-black">{{dropTitle( acao.attributes.titulo)}}</div>
+        <div class="text-subtitle2 text-black">{{acao.attributes.coordenador_da_acao_de_extensao}}</div>
+      </q-card-section>
+
+
+        <!-- <q-img class='rounded-borders' v-if='vaga == true'  :src="'images/vagas'+(index%3+1)">
       <p class="q-ml-xl q-pl-sm q-mt-md q-pt-md q-pr-sm text-left text-h6" :style='"border-left: 3px solid "+ meta[acao.attributes.modalidade].corTitulo +";" + "color: white"'>
         {{ dropTitle(acao.attributes.titulo) }}
       </p>
@@ -24,7 +20,7 @@
       <p class="q-ml-xl q-mt-xl q-pt-xl q-pl-sm  text-left text-h7" :style='"color: "+ meta[acao.attributes.modalidade].cor'>
         Endereço: {{ acao.attributes.endereco }}
       </p>
-    </q-img>
+    </q-img> -->
     </router-link>
   </q-card>
 </template>
@@ -66,7 +62,7 @@ export default defineComponent({
 
   methods: {
     dropTitle(titulo){
-      return titulo.lenth < 80 ? titulo : titulo.slice(0,80) + ' ...'
+      return titulo.length < 70 ? titulo : (titulo.slice(0,70) + ' ...')
     }
   },
 
@@ -75,8 +71,9 @@ export default defineComponent({
 
 <style scoped>
 
-
-.rounded-borders {
-  border-radius: 5%;
+.mycard{
+  width: 300px;
+  max-width: 300px;
 }
+
 </style>
--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -1,125 +1,173 @@
 <template>
-    <q-page class="hero">
-      <p class='text-h5 text-bold q-px-md q-my-sm'><span style='color: magenta;'>>></span> Em destaque</p>
-      <q-carousel
-        v-model="slide"
-        transition-prev="jump-right"
-        transition-next="jump-left"
-        swipeable
-        animated
-        control-color="white"
-        prev-icon="arrow_left"
-        next-icon="arrow_right"
-        navigation-icon="radio_button_unchecked"
-        navigation
-        padding
-        arrows
-        class="bg-purple shadow-1 "
+  <q-page class="hero">
+    <p class="text-h5 text-bold q-px-xl q-my-md">
+      <span style="color: magenta">>></span> Em destaque
+    </p>
+    <q-carousel
+      v-model="slide"
+      transition-prev="jump-right"
+      transition-next="jump-left"
+      swipeable
+      animated
+      control-color="white"
+      prev-icon="arrow_left"
+      next-icon="arrow_right"
+      navigation-icon="radio_button_unchecked"
+      navigation
+      padding
+      arrows
+      class="bg-purple shadow-1"
+    >
+      <q-carousel-slide
+        name="style"
+        img-src="images/comunica-01-2022.png"
+        class="row no-wrap slideimage"
       >
-        <q-carousel-slide
-          name="style"
-          img-src="images/comunica-01-2022.png"
-          class="row no-wrap slideimage"
-        >
-          <div class="q-ma-xl custom-caption absolute-bottom-right ">
-            <q-btn color='purple' label='Inscrever' :to="{ name: 'Comunica'}"> </q-btn>
+        <div class="q-ma-xl custom-caption absolute-bottom-right">
+          <q-btn color="purple" label="Inscrever" :to="{ name: 'Comunica' }">
+          </q-btn>
         </div>
-
-        </q-carousel-slide>
-          <q-carousel-slide
-          name="podcast"
-          img-src="images/podcast.jpg"
-          class="slideimage row no-wrap"
-        >
-          <div class="q-ma-xl custom-caption absolute-bottom-left">
-              <q-btn
-                class="col-1"
-                flat
-                icon="img:https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg"
-              />
-              <q-btn
-                class="col-1"
-                flat
-                icon="img:https://upload.wikimedia.org/wikipedia/commons/e/e7/Podcasts_%28iOS%29.svg"
-              />
-            </div>
-
-
-        </q-carousel-slide>
-      
-      </q-carousel>
-    </q-page>
-
-    <q-page ref="destaque" id="destaque" class=" ">
-      <p class="q-px-md q-my-md  text-h5 text-bold"> <span style='color: magenta;'>>></span> Descubra o que fazer em ABRIL</p>
-      <div class="col row q-pa-sm justify-around">
-        <div class="col-auto q-ma-sm" v-for="(projeto, index) in projetos" :key="projeto">
-          <home-card :vaga='false' :acao="projeto" :index='index'> </home-card>
-        </div>
-      </div>
-      <q-btn
-        v-on:click="this.$router.push('/acoes')"
-        class="col-1"
-        label="Veja todas as ações"
-      ></q-btn>
-    </q-page>
-
-    <q-page class="column bg-yellow q-pa-xl">
-      <span class="col-1 text-h2">Vagas em Destaque</span>
-      <div class="col row q-pa-sm justify-around">
-        <div class="col-auto q-ma-sm" v-for="(projeto, index) in vagas" :key="projeto">
-          <home-card :vaga='true' :acao="projeto" :index='index'> </home-card>
-        </div>
-      </div>
-      <q-btn
-        v-on:click="this.$router.push('/acoes')"
-        class="col-1 q-mt-xl"
-        label="Veja todas as vagas"
-      ></q-btn>
-    </q-page>
-
-    <q-page class="column bg-green q-pa-xl">
-      <span class="col-1 text-h2">Lives em Destaque</span>
-      <q-tabs class="col-1">
-        <q-tab>Ao vivo</q-tab>
-        <q-tab>Próximas</q-tab>
-        <q-tab>Passadas</q-tab>
-      </q-tabs>
-      <div class="col row q-pa-xl  justify-around">
-        <div class="col-3 q-ma-xl" v-for="acao in acoes" :key="acao">
-          <q-card
+      </q-carousel-slide>
+      <q-carousel-slide
+        name="podcast"
+        img-src="images/podcast.jpg"
+        class="slideimage row no-wrap"
+      >
+        <div class="q-ma-xl custom-caption absolute-bottom-left">
+          <q-btn
+            class="col-1"
             flat
-            vertical
-            class="my-card"
-            :style="'border-color: ' + acao.cor"
-          >
-            <iframe
-              width="400"
-              height="315"
-              src="https://www.youtube.com/embed/Xz3bMCj6BFI"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-
-            <q-card-section>
-              <div class="text-h6">{{ acao.titulo }}</div>
-              <div class="text-subtitle2">{{ acao.coordenador }}</div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              {{ acao.descricao }}
-            </q-card-section>
-          </q-card>
+            icon="img:https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg"
+          />
+          <q-btn
+            class="col-1"
+            flat
+            icon="img:https://upload.wikimedia.org/wikipedia/commons/e/e7/Podcasts_%28iOS%29.svg"
+          />
         </div>
+      </q-carousel-slide>
+    </q-carousel>
+  </q-page>
+
+  <q-page ref="destaque" id="destaque" class="q-pa-xl">
+    <p class="q-px-sm text-h5 text-bold">
+      <span style="color: magenta">>></span> Descubra o que fazer em ABRIL
+    </p>
+      <vue-horizontal responsive>
+      <section
+        class="col-auto q-ma-md"
+        v-for="(projeto, index) in projetos"
+        :key="projeto"
+      >
+        <home-card :vaga="false" :acao="projeto" :index="index"> </home-card>
+      </section>
+  </vue-horizontal>
+
+  <q-input class="q-pa-xl" style="max-width: 500px"
+        v-model="acoessearch"
+        debounce="1000"
+        filled
+        placeholder="Pesquise sua ação"
+      >
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+    <div class="col row q-pa-sm justify-around">
+    <q-btn flat
+      v-on:click="this.$router.push('/acoes')"
+      class=""
+      label="Veja todas as ações"
+    ><span style="color:blue" class="q-px-sm"> V</span> </q-btn>
+    </div>
+
+  </q-page>
+
+  <q-page ref="destaque" id="destaque" class="q-pa-xl">
+    <p class="q-px-sm text-h5 text-bold">
+      <span style="color: magenta">>></span> VAGAS em Ações de Extensão
+    </p>
+      <vue-horizontal responsive>
+      <div
+        class="col-auto q-ma-md"
+        v-for="(projeto, index) in vagas"
+        :key="projeto"
+      >
+        <home-card :vaga="false" :acao="projeto" :index="index"> </home-card>
       </div>
-      <q-btn
-        v-on:click="this.$router.push('/lives')"
-        class="col-1"
-        label="Veja todas as Lives"
-      ></q-btn>
-    </q-page>
+  </vue-horizontal>
+
+  <q-input class="q-pa-xl" style="max-width: 500px"
+        v-model="acoessearch"
+        debounce="1000"
+        filled
+        placeholder="Pesquise sua vaga"
+      >
+        <template v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+    <div class="col row q-pa-sm justify-around">
+    <q-btn flat
+      v-on:click="this.$router.push('/acoes')"
+      class=""
+      label="Veja todas as vagas"
+    ><span style="color:blue" class="q-px-sm"> V</span> </q-btn>
+    </div>
+
+  </q-page>
+
+
+  <q-page class="column bg-lime q-pa-xl">
+    <div class="row">
+    <p class="col q-px-sm text-h5 text-bold">
+      <span style="color: magenta">>></span> Lives em Destaque
+    </p>
+        <q-tabs class="col">
+      <q-tab>Ao vivo</q-tab>
+      <q-tab>Próximas</q-tab>
+      <q-tab>Passadas</q-tab>
+    </q-tabs>
+    </div>
+
+
+    <div class="col row q-pa-xl justify-around">
+      <div class="col-3 q-ma-xl" v-for="acao in acoes" :key="acao">
+        <q-card
+          flat
+          vertical
+          class="my-card"
+          :style="'border-color: ' + acao.cor"
+        >
+          <iframe
+            width="400"
+            height="315"
+            src="https://www.youtube.com/embed/Xz3bMCj6BFI"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+
+          <q-card-section>
+            <div class="text-h6">{{ acao.titulo }}</div>
+            <div class="text-subtitle2">{{ acao.coordenador }}</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            {{ acao.descricao }}
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+    <q-btn flat
+      v-on:click="this.$router.push('/lives')"
+      class="col-1"
+      label="Carregar mais"
+    ></q-btn>
+  </q-page>
 </template>
 
 <script>
@@ -128,16 +176,20 @@ import "@quasar/extras/fontawesome-v5";
 import axios from "axios";
 import HomeCard from "../components/HomeCard.vue";
 import json from "../../public/home.json";
+import VueHorizontal from "vue-horizontal";
 
 export default defineComponent({
-  components: { HomeCard },
+  components: { HomeCard, VueHorizontal },
   name: "PageIndex",
   data: function () {
     return {
+      acoessearch : '',
       slide: "style",
       projetos: [],
-      vagas:[],
-
+      vagas: [],
+      items: [...Array(20).keys()].map((i) => {
+        return { title: `Item ${i}`, content: `🚀 Content ${i}` };
+      }),
       acoes: [
         {
           titulo: "América Latina Póscolonial: estudos de caso.",
@@ -168,19 +220,19 @@ export default defineComponent({
       ],
     };
   },
-  created(){
-    this.windowHeight = window.innerHeight + 'px'
+  created() {
+    this.windowHeight = window.innerHeight + "px";
   },
   methods: {
     getProjetos() {
       axios.get("http://127.0.0.1:8000/home/").then((response) => {
         (this.projetos = response.data.data), console.log(this.projetos);
-        this.vagas = response.data.data.slice(0,3)
+        this.vagas = response.data.data.slice(0, 3);
       });
     },
-    getProjetosLocal(){
-      this.projetos = json.data
-      this.vagas = this.projetos.slice(0,3)
+    getProjetosLocal() {
+      this.projetos = json.data;
+      this.vagas = this.projetos.slice(0, 3);
     },
   },
   mounted() {
@@ -190,7 +242,6 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
 .q-carousel {
   min-height: inherit;
   min-width: inherit;
@@ -206,14 +257,15 @@ export default defineComponent({
   min-height: 400px;
 }
 
-.text-shadow-black{
+.text-shadow-black {
   text-shadow: 1px 1px 2px black;
 }
 
-
-.slideimage{
-    background-size:cover;
-    background-repeat: no-repeat;
-    background-position: center;
+.slideimage {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
 }
+
+
 </style>
